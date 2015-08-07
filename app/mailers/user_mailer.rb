ec4983d9f3fedef4334e3436a9b95890bb7b5057
root <kolja.schneider1125@gmail.com>
2015-08-07 21:12:29 +0200

    sign up

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,9 +1,9 @@
 class UserMailer < ActionMailer::Base
-    default from: "Harry's <welcome@harrys.com>"
+    default from: "Daniel's <daniel@nomessproducts.com>"
 
     def signup_email(user)
         @user = user
-        @twitter_message = "#Shaving is evolving. Excited for @harrys to launch."
+        @twitter_message = "#No Mess Products."
 
         mail(:to => user.email, :subject => "Thanks for signing up!")
     end
